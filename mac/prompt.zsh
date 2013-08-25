aa0c11dffe18b7412907393f997613e57a1d9fe7
Yu Sawai <yusawai.null@gmail.com>
2013-08-25 23:57:14 +0900

    Mac: Migrated from Oh-my-zsh to Prezto

---
+++ mac/prompt.zsh
@@ -0,0 +1,48 @@
+#-------------------------------------------------------------------------------
+# PROMPT setting                                                                
+#-------------------------------------------------------------------------------
+USERNAME=whoami
+function collapsed_pwd {
+    echo $(pwd | sed -e "s,^$HOME,~,")
+}
+
+function prompt_char {
+    git branch >/dev/null 2>/dev/null && echo '+++' && return
+    hg root >/dev/null 2>/dev/null && echo '---' && return
+    echo '>>>'
+}
+
+function virtualenv_info {
+    [ $VIRTUAL_ENV ] && echo ''`basename $VIRTUAL_ENV`''
+}
+
+zle -N zle-line-init
+zle -N zle-keymap-select
+
+autoload -Uz colors && colors
+PROMPT='
+%{$fg_bold[blue]%}%n@%m%{$reset_color%} in %{$fg_bold[blue]%}$(collapsed_pwd) :: $(virtualenv_info) %{$reset_color%}$(git_prompt_info)
+%{$fg[blue]%}$(prompt_char)%{$reset_color%} '
+
+function zle-line-init zle-keymap-select {
+  case $KEYMAP in
+    vicmd)
+PROMPT='
+%{$fg_bold[blue]%}%n@%m%{$reset_color%} in %{$fg_bold[blue]%}$(collapsed_pwd) :: $(virtualenv_info) %{$reset_color%}$(git_prompt_info)
+%{$fg[magenta]%}$(prompt_char)%{$reset_color%} '
+    ;;
+    main|viins)
+PROMPT='
+%{$fg_bold[blue]%}%n@%m%{$reset_color%} in %{$fg_bold[blue]%}$(collapsed_pwd) :: $(virtualenv_info) %{$reset_color%}$(git_prompt_info)
+%{$fg[blue]%}$(prompt_char)%{$reset_color%} '
+    ;;
+  esac
+  zle reset-prompt
+}
+
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[magenta]%}["
+ZSH_THEME_GIT_PROMPT_SUFFIX="]%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[magenta]%}!!"
+ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[magenta]%}??"
+ZSH_THEME_GIT_PROMPT_CLEAN=""
+
