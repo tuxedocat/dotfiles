af3c0d9764a8e589e3b1330765b1577902861242
tuxedocat <tuxedocat@users.noreply.github.com>
2019-01-17 10:46:25 +0900

    Change font application order, disable cursor blink

--- mac/hyper.js
+++ mac/hyper.js
@@ -6,7 +6,7 @@ module.exports = {
         lineHeight: 1.2,
 
         // font family with optional fallbacks
-        fontFamily: '"Source Han Code JP","Source Code Pro"',
+        fontFamily: '"Source Code Pro","Source Han Code JP"',
         fontWeight: '400',
         fontWeightBold: '700',
 
@@ -15,7 +15,7 @@ module.exports = {
 
         // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
         cursorShape: 'UNDERLINE',
-        cursorBlink: true,
+        cursorBlink: false,
 
         // color of the text
         foregroundColor: '#1d252c',
@@ -39,24 +39,24 @@ module.exports = {
         // the full list. if you're going to provide the full color palette,
         // including the 6 x 6 color cubes and the grayscale map, just provide
         // an array here instead of a color map object
-        colors: {
-            black: '#000000',
-            red: '#ff0000',
-            green: '#33ff00',
-            yellow: '#ffff00',
-            blue: '#0066ff',
-            magenta: '#cc00ff',
-            cyan: '#00ffff',
-            white: '#d0d0d0',
-            lightBlack: '#808080',
-            lightRed: '#ff0000',
-            lightGreen: '#33ff00',
-            lightYellow: '#ffff00',
-            lightBlue: '#0066ff',
-            lightMagenta: '#cc00ff',
-            lightCyan: '#00ffff',
-            lightWhite: '#ffffff'
-        },
+        // colors: {
+        //     black: '#000000',
+        //     red: '#ff0000',
+        //     green: '#33ff00',
+        //     yellow: '#ffff00',
+        //     blue: '#0066ff',
+        //     magenta: '#cc00ff',
+        //     cyan: '#00ffff',
+        //     white: '#d0d0d0',
+        //     lightBlack: '#808080',
+        //     lightRed: '#ff0000',
+        //     lightGreen: '#33ff00',
+        //     lightYellow: '#ffff00',
+        //     lightBlue: '#0066ff',
+        //     lightMagenta: '#cc00ff',
+        //     lightCyan: '#00ffff',
+        //     lightWhite: '#ffffff'
+        // },
 
         // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
         // if left empty, your system's login shell will be used by default
@@ -75,7 +75,7 @@ module.exports = {
         bell: 'SOUND',
 
         // if true, selected text will automatically be copied to the clipboard
-        copyOnSelect: false
+        copyOnSelect: true
 
         // URL to custom bell
         // bellSoundURL: 'http://example.com/bell.mp3',
